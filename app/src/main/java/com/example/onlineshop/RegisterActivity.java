3a4968fc732f1cd5a6beb48cd97598d60ec4d498
adrian-sz <adrianszczesek@gmail.com>
2020-11-21 22:13:43 +0100

    połączenie aktywności RegisterActivity z bazą danych firebase

--- app/src/main/java/com/example/onlineshop/RegisterActivity.java
+++ app/src/main/java/com/example/onlineshop/RegisterActivity.java
@@ -1,8 +1,10 @@
 package com.example.onlineshop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -10,8 +12,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -47,15 +56,15 @@ public class RegisterActivity extends AppCompatActivity {
 
         if(TextUtils.isEmpty(name))
         {
-            Toast.makeText(this,"Proszę wpisać nazwę użytkownika!", Toast.LENGTH_SHORT);
+            Toast.makeText(this,"Proszę wpisać nazwę użytkownika!", Toast.LENGTH_SHORT).show();
         }
         else if(TextUtils.isEmpty(phone))
         {
-            Toast.makeText(this,"Proszę wpisać numer telefonu!", Toast.LENGTH_SHORT);
+            Toast.makeText(this,"Proszę wpisać numer telefonu!", Toast.LENGTH_SHORT).show();
         }
         else if(TextUtils.isEmpty(password))
         {
-            Toast.makeText(this,"Proszę wpisać hasło!", Toast.LENGTH_SHORT);
+            Toast.makeText(this,"Proszę wpisać hasło!", Toast.LENGTH_SHORT).show();
         }
         else
         {
@@ -68,9 +77,54 @@ public class RegisterActivity extends AppCompatActivity {
         }
     }
 
-    private void ValidatePhoneNumber(String name, String phone, String password) {
-       final DatabaseReference RootRef;
-       RootRef = FirebaseDatabase.getInstance().getReference();
+    private void ValidatePhoneNumber(String name, final String phone, String password) {
+        final DatabaseReference RootRef;
+        RootRef = FirebaseDatabase.getInstance().getReference();
+
+        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (!(snapshot.child("Users").child(phone).exists()))
+                {
+                    HashMap<String,Object> userdataMap = new HashMap<>();
+                    userdataMap.put("phone",phone);
+                    userdataMap.put("password",password);
+                    userdataMap.put("name",name);
+
+                    RootRef.child("Users").child(phone).updateChildren(userdataMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful())
+                            {
+                                Toast.makeText(RegisterActivity.this,"Twoje konto zostało założone!",Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+
+                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                startActivity(intent);
+                            }
+                            else {
+                                loadingBar.dismiss();
+                                Toast.makeText(RegisterActivity.this,"Bład, spróbuj ponownie!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+                }else
+                {
+                    Toast.makeText(RegisterActivity.this,"Ten " + phone + " numer już istnieje w bazie.", Toast.LENGTH_SHORT).show();
+                    loadingBar.dismiss();
+                    Toast.makeText(RegisterActivity.this,"Spróbuj użyć innego numeru.", Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
 }
