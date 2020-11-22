3b2e14b99165083901f97451fa52c6dbef6576f4
Aleksandra <olapawelec13@gmail.com>
2020-11-22 18:34:48 +0100

    Dokończenie LoginActivity i połączenie z MainActivity. Dodanie działalności "pamiętaj mnie".

--- app/src/main/java/com/example/onlineshop/LoginActivity.java
+++ app/src/main/java/com/example/onlineshop/LoginActivity.java
@@ -1,20 +1,37 @@
 package com.example.onlineshop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.renderscript.Sampler;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.onlineshop.Model.Users;
+import com.example.onlineshop.Prevalent.Prevalent;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.rey.material.widget.CheckBox;
+
+import io.paperdb.Paper;
+
 public class LoginActivity extends AppCompatActivity {
 
     private EditText InputNumber, InputPassword;
     private Button LoginButton;
     private ProgressDialog loadingBar;
+
+    private String parentDbname = "Users";
+    private com.rey.material.widget.CheckBox chkBoxRememberMe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +42,8 @@ public class LoginActivity extends AppCompatActivity {
         InputPassword = (EditText) findViewById(R.id.login_password_input);
         InputNumber = (EditText) findViewById(R.id.login_phone_number_input);
         loadingBar = new ProgressDialog(this);
+        chkBoxRememberMe = (CheckBox) findViewById(R.id.remember_me_chkb);
+        Paper.init(this);
 
         LoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,7 +68,7 @@ public class LoginActivity extends AppCompatActivity {
         }
         else {
 
-            loadingBar.setTitle("Stwórz konto");
+            loadingBar.setTitle("Trwa logowanie");
              loadingBar.setMessage("Proszę czekać, trwa weryfikcja");
              loadingBar.setCanceledOnTouchOutside(false);
              loadingBar.show();
@@ -59,6 +78,48 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void AllowAccessToAccount(String phone, String password) {
+
+        if(chkBoxRememberMe.isChecked()) {
+            Paper.book().write(Prevalent.UserPhoneKey, phone);
+            Paper.book().write(Prevalent.UserPasswordKey,password);
+        }
+
+        final DatabaseReference RootRef;
+        RootRef = FirebaseDatabase.getInstance().getReference();
+
+        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.child(parentDbname).child(phone).exists())
+                {
+                    Users usersData = snapshot.child(parentDbname).child(phone).getValue(Users.class);
+
+                    if( usersData.getPhone().equals(phone)) {
+                        if( usersData.getPassword().equals(password)) {
+
+                            Toast.makeText(LoginActivity.this,"Zalogowano pomyślnie!", Toast.LENGTH_SHORT).show();
+                            loadingBar.dismiss();
+
+                            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                            startActivity(intent);
+
+                        } else {
+                            loadingBar.dismiss();
+                            Toast.makeText(LoginActivity.this,"Błędne hasło", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }else {
+                    Toast.makeText(LoginActivity.this,"Konto z tym numerem telefonu nie istnieje!", Toast.LENGTH_SHORT).show();
+                    loadingBar.dismiss();
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
 
     }
--- app/src/main/java/com/example/onlineshop/MainActivity.java
+++ app/src/main/java/com/example/onlineshop/MainActivity.java
@@ -1,17 +1,34 @@
 package com.example.onlineshop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.example.onlineshop.Model.Users;
+import com.example.onlineshop.Prevalent.Prevalent;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import io.paperdb.Paper;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button joinNowButton, loginButton;
 
     private Button btn, btn2;
+
+    private ProgressDialog loadingBar;
 
 
     @Override
@@ -21,6 +38,9 @@ public class MainActivity extends AppCompatActivity {
 
         joinNowButton = (Button) findViewById(R.id.main_join_now_btn);
         loginButton = (Button) findViewById(R.id.main_login_btn);
+        loadingBar = new ProgressDialog(this);
+
+        Paper.init(this);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,6 +55,58 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
                 startActivity(intent);
+            }
+        });
+        String UserPhoneKey = Paper.book().read(Prevalent.UserPhoneKey);
+        String UserPasswordKey = Paper.book().read(Prevalent.UserPasswordKey);
+        if( UserPhoneKey != "" && UserPasswordKey != "" ) {
+            if(!TextUtils.isEmpty(UserPhoneKey) && !TextUtils.isEmpty(UserPasswordKey) ) {
+                AllowAcces(UserPhoneKey, UserPasswordKey);
+
+                loadingBar.setTitle("Już zalogowano");
+                loadingBar.setMessage("Proszę czekać...");
+                loadingBar.setCanceledOnTouchOutside(false);
+                loadingBar.show();
+            }
+        }
+    }
+
+    private void AllowAcces(String phone, String password) {
+
+        final DatabaseReference RootRef;
+        RootRef = FirebaseDatabase.getInstance().getReference();
+
+        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.child("Users").child(phone).exists())
+                {
+                    Users usersData = snapshot.child("Users").child(phone).getValue(Users.class);
+
+                    if( usersData.getPhone().equals(phone)) {
+                        if( usersData.getPassword().equals(password)) {
+
+                            Toast.makeText(MainActivity.this,"Zalogowano pomyślnie!", Toast.LENGTH_SHORT).show();
+                            loadingBar.dismiss();
+
+                            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                            startActivity(intent);
+
+                        } else {
+                            loadingBar.dismiss();
+                            Toast.makeText(MainActivity.this,"Błędne hasło", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }else {
+                    Toast.makeText(MainActivity.this,"Konto z tym numerem telefonu nie istnieje!", Toast.LENGTH_SHORT).show();
+                    loadingBar.dismiss();
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
     }
